97615a1f51f4e409073ab313e0a35c42219c7284
ode_Gabriel <odegabriel5@gmail.com>
2022-04-01 13:52:29 +0100

    removed same less important lines

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cookies = require('cookie-parser')
 const expressSession = require('express-session')
 const local = require('passport-local')
 
-//const User = require('./models/userModel')
 
 mongoose.connect("mongodb://127.0.0.1/myapp",{
   useUnifiedTopology : true,
@@ -30,40 +29,8 @@ app.use(expressSession(
     resave: true, 
     saveUninitialized: true
 }))
-// app.use(passport.initialize)
-// app.use(passport.session)
-// app.use(local())
-
-// passport.use(new LocalStrategy(
-//     function(username, password, done) {
-//       User.findOne({ username: username }, function (err, user) {
-//         if (err) { return done(err); }
-//         if (!user) { return done(null, false); }
-//         if (!user.verifyPassword(password)) { return done(null, false); }
-//         return done(null, user);
-//       });
-//     }
-//   ));
-
-//   passport.serializeUser(function(user, done) {
-//     done(null, user.id);
-//   });
-  
-//   passport.deserializeUser(function(id, done) {
-//     User.findById(id, function (err, user) {
-//       done(err, user);
-//     });
-//   });
-  //app.post('/login', 
-  //passport.authenticate('local', { failureRedirect: '/login' }),
-  //function(req, res) {
-    //  let {userName,password} = req.body
-      //console.log(userName)
-    //res.redirect('/');
-  //});
  app.use(router);
 
-// app.post('/login',(req)=>console.log(req.body.userName))
 
 app.listen(3002, ()=> console.log("server is running"))
 
